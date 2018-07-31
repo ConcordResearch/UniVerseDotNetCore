c7b047e73504e757b4bdcbc5a4b9e3d1a5d956a0
DESKTOP-CM4DN2T\joncatlin <jonc@destini.com>
2018-07-30 21:23:58 -0700

    Changed the structures used to store the developers, projects and lenders so that Appian can correctly read them in and decode them.

--- Controllers/HierarchyController.cs
+++ Controllers/HierarchyController.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using UniVerseDotNetCore.CSSLayer;
-using UniVerseDotNetCore.Models;
 
 namespace UniVerseDotNetCore.Controllers
 {
@@ -11,44 +9,58 @@ namespace UniVerseDotNetCore.Controllers
     [ApiController]
     public class HierarchyController : ControllerBase
     {
+        public class Node
+        {
+            public Node(string id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+
         // Initialize the data for the demo. These should be removed if we can get the Developers, Projects and Lenders from the Universe DB
-        static Dictionary<string, string> Developers = new Dictionary<string, string>()
+        static Node[] Developers = new Node[]
         {
-            { "0001", "Developer 0001" },
-            { "0002", "Developer 0002" },
-            { "0003", "Developer 0003" },
-            { "0004", "Developer 0004" },
-            { "0005", "Developer 0005" },
-            { "0006", "Developer 0006" },
-            { "0007", "Developer 0007" },
-            { "0008", "Developer 0008" },
-            { "0009", "Developer 0009" }
+            new Node("0001", "Developer 0001"),
+            new Node("0002", "Developer 0002"),
+            new Node("0003", "Developer 0003"),
+            new Node("0004", "Developer 0004"),
+            new Node("0005", "Developer 0005"),
+            new Node("0006", "Developer 0006"),
+            new Node("0007", "Developer 0007"),
+            new Node("0008", "Developer 0008"),
+            new Node("0009", "Developer 0009")
         };
-        static Dictionary<string, string> Projects = new Dictionary<string, string>()
+        static Node[] Projects = new Node[]
         {
-            { "010", "Project 010" },
-            { "020", "Project 020" },
-            { "030", "Project 030" },
-            { "040", "Project 040" },
-            { "050", "Project 050" },
-            { "060", "Project 060" },
-            { "070", "Project 070" }
+            new Node("010", "Project 010"),
+            new Node("020", "Project 020"),
+            new Node("030", "Project 030"),
+            new Node("040", "Project 040"),
+            new Node("050", "Project 050"),
+            new Node("060", "Project 060"),
+            new Node("070", "Project 070")
         };
-        static Dictionary<string, string> Lenders = new Dictionary<string, string>()
+        static Node[] Lenders = new Node[]
         {
-            { "100", "Lender 100" },
-            { "200", "Lender 200" },
-            { "300", "Lender 300" },
-            { "400", "Lender 400" }
+            new Node("100", "Lender 100"),
+            new Node("200", "Lender 200"),
+            new Node("300", "Lender 300"),
+            new Node("400", "Lender 400")
         };
 
         /// <summary>
         /// Retrieves the set of Developers that the user has access to
         /// </summary>
         /// <returns>Json array of the Developer codes and the text names of the Developers.</returns>
-        [HttpGet]
+        [HttpGet("developers")]
         public string GetDevelopers()
         {
+            Console.WriteLine($"Received request for developers");
+
             return JsonConvert.SerializeObject(Developers);
         }
 
@@ -56,19 +68,25 @@ namespace UniVerseDotNetCore.Controllers
         /// Retrieves the set of Projects that the user has access to
         /// </summary>
         /// <returns>Json array of the Lender codes and the text names of the Projects.</returns>
-        [HttpGet]
-        public string GetProjects()
+        [HttpGet("projects")]
+        public string GetProjects([FromQuery(Name = "developers")] string developers)
         {
+            var jsonDevelopers = JsonConvert.SerializeObject(developers);
+            Console.WriteLine($"Received request for projects given the developers: {jsonDevelopers}");
+
             return JsonConvert.SerializeObject(Projects);
         }
-         
+
         /// <summary>
         /// Retrieves the set of Lenders that the user has access to
         /// </summary>
         /// <returns>Json array of the Lender codes and the text names of the Lenders.</returns>
-        [HttpGet]
-        public string GetLenders()
+        [HttpGet("lenders")]
+        public string GetLenders([FromQuery(Name = "developers")] string developers)
         {
+            var jsonDevelopers = JsonConvert.SerializeObject(developers);
+            Console.WriteLine($"Received request for projects given the developers: {jsonDevelopers}");
+
             return JsonConvert.SerializeObject(Lenders);
         }
 
