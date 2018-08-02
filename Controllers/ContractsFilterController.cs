1ecc093fdb90430f518f188f1473e62f834a6326
Alfredo Herrera <alfredherr@gmail.com>
2018-08-01 17:05:58 -0700

    updated ContractsFilter default GET response to use a loan DPL in criteria

--- Controllers/ContractsFilterController.cs
+++ Controllers/ContractsFilterController.cs
@@ -5,55 +5,48 @@ using UniVerseDotNetCore.Domain.CssServiceLayer.Models;
 using UniVerseDotNetCore.Domain.Models;
 using UniVerseDotNetCore.Models;
 
-
-namespace UniVerseDotNetCore.Controllers
-{
-    [Route("api/[controller]")]
+namespace UniVerseDotNetCore.Controllers {
+    [Route ("api/[controller]")]
     [ApiController]
-    public class ContractsFilterController : ControllerBase
-    {
-      
+    public class ContractsFilterController : ControllerBase {
+
         [HttpGet]
-        public JsonResult GetContractsFileModel()
-        {
-            var filtrCriteria = new AccountFilterCriteria<Contracts>();
-            filtrCriteria.AddCriterion(new Criterion() { Attribute = "DEV.CODE", Filter = "1075" });
-            var credentials = new CssCredentialsModel();
-            var model = new ContractsFilterCallModel(){Credentials = credentials,FilterCriteria = filtrCriteria};
-            return new JsonResult(model);
+        public JsonResult GetContractsFileModel () {
+            var filtrCriteria = new AccountFilterCriteria<Contracts> ();
+            filtrCriteria.AddCriterion (new Criterion () { Attribute = "DEV.CODE", Filter = "6600" });
+            filtrCriteria.AddCriterion (new Criterion () { Attribute = "PROJ.CODE", Filter = "6606" });
+            filtrCriteria.AddCriterion (new Criterion () { Attribute = "LEND.CODE", Filter = "100" });
+            filtrCriteria.AddCriterion (new Criterion () { Attribute = "AC", Filter = "N" });
+
+            var credentials = new CssCredentialsModel ();
+            var model = new ContractsFilterCallModel () { Credentials = credentials, FilterCriteria = filtrCriteria };
+            return new JsonResult (model);
         }
-    
-        [HttpPost] 
-        public JsonResult ReturnContractsFile([FromBody] ContractsFilterCallModel model)
-        {
-            
-            var usernameSplit = model.Credentials.User.Split("\\");
-            var username = usernameSplit.Length == 2 ? usernameSplit[1].ToUpper() : "unknown";
+
+        [HttpPost]
+        public JsonResult ReturnContractsFile ([FromBody] ContractsFilterCallModel model) {
+
+            var usernameSplit = model.Credentials.User.Split ("\\");
+            var username = usernameSplit.Length == 2 ? usernameSplit[1].ToUpper () : "unknown";
 
             var listname = $"{username}.{model.FilterCriteria.CssFileName.ToString().ToUpper()}.{Utils.GetRandomString()}";
-            var name = new AccountList(){AccountListName = listname};
+            var name = new AccountList () { AccountListName = listname };
 
-            if (CssAppConfig.RunInTestMode)
-            {
-                var results = new CssCommandResult()
-                {
-                    Results = new List<CommandResponse>()
-                    {
-                        new CommandResponse("sample css action","sample css response"),
-                        new CommandResponse("sample css action","sample css response")
+            if (CssAppConfig.RunInTestMode) {
+                var results = new CssCommandResult () {
+                    Results = new List<CommandResponse> () {
+                    new CommandResponse ("sample css action", "sample css response"),
+                    new CommandResponse ("sample css action", "sample css response")
                     }
                 };
-                return new JsonResult(new { ListName = name.ToString(), FilterResultLog = results });
+                return new JsonResult (new { ListName = name.ToString (), FilterResultLog = results });
             }
 
+            CssCommandResult data = FilterCapability.FilterContracts (model.FilterCriteria, name, model.Credentials);
 
-            CssCommandResult data = FilterCapability.FilterContracts(model.FilterCriteria, name, model.Credentials);
-
-            return new JsonResult(new {ListName = name.ToString(), FilterResultLog = data});
-           
+            return new JsonResult (new { ListName = name.ToString (), FilterResultLog = data });
 
         }
 
-       
     }
 }
